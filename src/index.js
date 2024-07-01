e7e5cf8d7d808f78291e3133568c3c4e32f3d00b
rahulkr-22 <rahul.kumar1@practo.com@github.com>
2024-07-01 07:06:10 +0530

    Add first loader to handle CSS

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import {run} from "./app/app"
 import { AlertService } from "./app/alert.service.js";
 import {ComponentService} from "./app/component.service.js"
+import "./main.css"
 
 const alertService = new AlertService();
 const componentService = new ComponentService();
